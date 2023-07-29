366af0ab622276114cc946d15f3a11582e8b1a5e
CodeSandbox <support@codesandbox.io>
2023-07-29 13:29:57 +0000

    "Initial commit"

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-xl font-bold mb-2">Vite + React</h1>
+      <h2>Edit this file to get started!</h2>
+    </div>
+  );
+}
+
+export default App;

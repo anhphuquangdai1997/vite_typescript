8d4fd33f1d8114822ebdd4835bb5e5357efb4492
CodeSandbox <support@codesandbox.io>
2023-12-05 06:47:07 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="p-4 text-center">
-      <h1 className="text-xl font-bold mb-2">Vite + React</h1>
-      <h2>Edit this file to get started!</h2>
-    </div>
-  );
+  return;
 }
 
 export default App;
